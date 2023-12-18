1c1b6b16f469cc970f759f13a5266afd1f749b32
user <panpanzov.nur@bk.ru>
2023-12-18 12:42:28 +0600

    add/, fix/food, bug/

--- apps/main_app/models.py
+++ apps/main_app/models.py
@@ -15,10 +15,10 @@ class Driver(models.Model):
 
 class Item(models.Model):
     transmitter_id = models.CharField(verbose_name='ID передатчика', max_length=50, null=True, blank=True)  # Добавленное поле
-    latitude = models.DecimalField(verbose_name='Широта', max_digits=9, decimal_places=6)  # Широта
-    longitude = models.DecimalField(verbose_name='Долгота', max_digits=9, decimal_places=6)  # Долгота
-    direction = models.DecimalField(verbose_name='Направление', max_digits=5, decimal_places=2)  # Направление
-    speed = models.DecimalField(verbose_name='Скорость', max_digits=5, decimal_places=2)  # Скорость
+    latitude = models.DecimalField(verbose_name='Широта', max_digits=9, decimal_places=6, null=True)  # Широта
+    longitude = models.DecimalField(verbose_name='Долгота', max_digits=9, decimal_places=6, null=True)  # Долгота
+    direction = models.DecimalField(verbose_name='Направление', max_digits=5, decimal_places=2, null=True)  # Направление
+    speed = models.DecimalField(verbose_name='Скорость', max_digits=5, decimal_places=2, null=True)  # Скорость
     timestamp = models.DateTimeField(verbose_name='Дата и время', auto_now_add=True)  
     driver = models.OneToOneField(Driver, on_delete=models.CASCADE, null=True, blank=True)  # Связь OneToOne с моделью Location
 
